4a88aaee66b19cd8c363a75772c6f43fc863e140
John Branch <jwbranch3@hotmail.com>
2024-07-17 14:30:15 -0400

    File select window for CSV file `refactor: Update PrimaryController to use WriteData instead of ReadData`

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,30 +1,23 @@
 package com.example;
 
+import java.io.File;
 import java.io.IOException;
 
-import com.example.data.DB;
 import com.example.data.WriteData;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 public class PrimaryController {
 
-     @FXML
-    private Label addedRecords;
-
     @FXML
     private VBox categoryBox;
-
-    @FXML
-    private CheckBox clearCat;
 
     @FXML
     private AnchorPane myAnchorPane;
@@ -36,17 +29,33 @@ public class PrimaryController {
     private ProgressIndicator progressIndicator;
 
     @FXML
-    private Label rejectedRecords;
+    void readActual(ActionEvent event) {
+        // routine to open dialog box to select file
+        String startFile = "";
+
+        startFile = "D:\\VSCwork\\budget\\";
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(startFile));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+
+        File selectedFile = fileChooser.showOpenDialog(primaryButton.getScene().getWindow());
+        if (selectedFile != null) {
+            // Process the selected file
+            String filePath = selectedFile.getAbsolutePath();
+            System.out.println("File Path: " + filePath);
+       }
+
+    }
 
     @FXML
     void readHeadings(ActionEvent event) {
         progressIndicator.setVisible(true);
         progressIndicator.setProgress(ProgressBar.INDETERMINATE_PROGRESS);
 
-        WriteData.readHeadings(DB.CSV_FILE, clearCat.isSelected());
         int temp = WriteData.getRejectedRecordCount();
-        rejectedRecords.setText(String.valueOf(temp));
-        addedRecords.setText(String.valueOf(WriteData.getAddedRecordCount()));
+        // rejectedRecords.setText(String.valueOf(temp));
+        // addedRecords.setText(String.valueOf(WriteData.getAddedRecordCount()));
 
         progressIndicator.setVisible(false);
 
